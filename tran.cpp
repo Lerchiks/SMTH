4fa4daaddbe416aedba802ee132fb591db2276f3
Valeria Dubibina <114979867+Lerchiks@users.noreply.github.com>
2022-10-25 11:54:19 +0300

    Create tran.cpp
    перевод в сс

---
+++ tran.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+int main()
+{
+	unsigned int n = 0;
+	std::cin >> n;
+	unsigned int  a = n, bin_a;
+	int su = 0, pr = 1;
+    unsigned int shift;	
+
+	while (a)
+	{
+		su += a%10;
+		pr *= a%10;
+		a /= 10;
+	}
+	std::cout << "sum = " << su << " pr = " << pr << std::endl;
+    
+    shift = 1;
+    while (shift < n)
+        shift *= 2;
+    
+    a = n;
+    
+    shift /= 2;
+    
+    while (shift)
+    {
+        std::cout << a / shift;
+        a %= shift;
+        shift /= 2;
+        
+    }
+
+//00111010101 
+	return 0;
+}
